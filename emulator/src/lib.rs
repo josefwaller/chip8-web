bd3534cf79b97e423052ed38fdbed93891389350
Josef Waller <josef@siriusapplications.com>
2024-07-28 14:53:18 -0700

    Render to canvas

--- emulator/src/lib.rs
+++ emulator/src/lib.rs
@@ -1,6 +1,7 @@
 use rust_chip8_opengl::processor::Processor;
 use std::{cell::RefCell, panic, rc::Rc};
 use wasm_bindgen::prelude::*;
+use web_sys::{HtmlCanvasElement, WebGl2RenderingContext, Window};
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,8 +9,24 @@ extern "C" {
     fn log(s: &str);
 }
 
-pub fn get_window() -> web_sys::Window {
+pub fn get_window() -> Window {
     web_sys::window().expect("no global `window` exists")
+}
+
+pub fn get_canvas() -> WebGl2RenderingContext {
+    let canvas = get_window()
+        .document()
+        .unwrap()
+        .get_element_by_id("canvas")
+        .unwrap();
+    canvas
+        .dyn_into::<HtmlCanvasElement>()
+        .unwrap()
+        .get_context("webgl2")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<WebGl2RenderingContext>()
+        .unwrap()
 }
 
 #[wasm_bindgen]
@@ -22,10 +39,15 @@ pub fn start_main_loop() {
 
     let mut i = 0;
 
+    let canvas = get_canvas();
+
     *r_clone.borrow_mut() = Some(Closure::new(move || {
         log("Main loop called");
 
         i += 1;
+
+        canvas.clear_color((i % 256) as f32 / 255.0, 0.0, 0.0, 1.0);
+        canvas.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
         get_window()
             .request_animation_frame(r.borrow().as_ref().unwrap().as_ref().unchecked_ref())
