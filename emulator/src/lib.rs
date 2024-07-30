39e90eb69fe9747ff3b0623d6da096b72130ecda
Josef Waller <josef@siriusapplications.com>
2024-07-29 19:43:12 -0700

    Add variable clock speed

--- emulator/src/lib.rs
+++ emulator/src/lib.rs
@@ -6,7 +6,7 @@ use wasm_bindgen::prelude::*;
 use web_gl::{
     buffer_data_f32, create_buffer_f32, create_buffer_i32, create_program, create_vertex_array,
 };
-use web_sys::{HtmlCanvasElement, WebGl2RenderingContext, Window};
+use web_sys::{HtmlCanvasElement, HtmlInputElement, WebGl2RenderingContext, Window};
 
 #[wasm_bindgen]
 extern "C" {
@@ -121,8 +121,8 @@ pub fn start_main_loop(rom: &[u8]) {
         .collect();
     // Create buffer
     let colors_buffer = create_buffer_f32(&context, WebGl2RenderingContext::ARRAY_BUFFER, colors);
-    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&colors_buffer));
     // Point GLSL 'color' variable to the array
+    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&colors_buffer));
     let cidx = context.get_attrib_location(&program, "color");
     context.vertex_attrib_pointer_with_i32(
         cidx as u32,
@@ -138,11 +138,42 @@ pub fn start_main_loop(rom: &[u8]) {
         .performance()
         .expect("Can't get performance!")
         .now();
+    let mut last_tick_time = last_time;
 
     *r_clone.borrow_mut() = Some(Closure::new(move || {
-        for _ in 0..100 {
+        let clock_speed = get_window()
+            .document()
+            .unwrap()
+            .get_element_by_id("clock-speed")
+            .unwrap()
+            .dyn_into::<HtmlInputElement>()
+            .unwrap()
+            .value_as_number();
+        let new_time = get_window()
+            .performance()
+            .expect("Can't get performance!")
+            .now();
+
+        if new_time - last_tick_time >= 1000.0 / 60.0 {
+            p.on_tick();
+            last_tick_time = new_time;
+        }
+        let dt = new_time - last_time;
+
+        let n_steps = (dt / 1000.0 * clock_speed) as u32;
+        for _ in 0..n_steps {
             p.step();
         }
+        // Uncomment this for logging
+        // log(format!(
+        //     "DT: {:?}, FPS: {:?}, N Steps: {:?}, Clock Speed: {:?}",
+        //     dt,
+        //     1000.0 / dt,
+        //     n_steps,
+        //     clock_speed
+        // )
+        // .as_str());
+        last_time = new_time;
 
         let mut new_colors = Vec::new();
         for x in 0..SCREEN_WIDTH {
@@ -170,15 +201,6 @@ pub fn start_main_loop(rom: &[u8]) {
             WebGl2RenderingContext::UNSIGNED_INT,
             0,
         );
-        let new_time = get_window()
-            .performance()
-            .expect("Can't get performance!")
-            .now();
-        if new_time - last_time >= 1000.0 / 60.0 {
-            p.on_tick();
-            last_time = new_time;
-        }
-
         get_window()
             .request_animation_frame(r.borrow().as_ref().unwrap().as_ref().unchecked_ref())
             .expect("Couldn't request animation frame");
